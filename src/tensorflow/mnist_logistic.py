211e9752c872c1114cda867fbc933fdead1ea893
Takuma Yamaguchi <kumon0587@gmail.com>
2019-05-17 15:37:15 +0900

    update

--- src/tensorflow/mnist_logistic.py
+++ src/tensorflow/mnist_logistic.py
@@ -6,9 +6,13 @@ LEARNING_RATE = 0.1
 TRAINING_LOOP = 20000
 BATCH_SIZE = 100
 SUMMARY_DIR = 'log_logistic'
-SUMMARY_INTERVAL = 100
+SUMMARY_INTERVAL = 1000
+EPS = 1e-10
 
 mnist = input_data.read_data_sets('data', one_hot=True)
+
+def gen_label(labels):
+    return list(map(lambda x: [1] if x[0] == 1 else [0], labels))
 
 with tf.Graph().as_default():
     with tf.name_scope('input'):
@@ -18,31 +22,35 @@ with tf.Graph().as_default():
     with tf.name_scope('readout'):
         W = weight_variable([IMAGE_SIZE, CATEGORY_NUM], name='weight')
         b = bias_variable([CATEGORY_NUM], name='bias')
-        y = tf.nn.sigmoid(tf.matmul(x, W) + b)
+        #y = tf.nn.sigmoid(tf.matmul(x, W) + b)
+        y = tf.clip_by_value(tf.nn.sigmoid(tf.matmul(x, W) + b), EPS, 1.0 - EPS)
 
     with tf.name_scope('optimize'):
         log_likelihood = tf.reduce_mean(tf.reduce_sum(y_ * tf.log(y) + (1 - y_) * tf.log(1 - y), reduction_indices=[1]))
         train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(-log_likelihood)
-        log_likelihood_summary = tf.scalar_summary('log likelihood', log_likelihood)
+        log_likelihood_summary = tf.summary.scalar('log likelihood', log_likelihood)
 
     with tf.Session() as sess:
-        train_writer = tf.train.SummaryWriter(SUMMARY_DIR + '/train', sess.graph)
-        test_writer = tf.train.SummaryWriter(SUMMARY_DIR + '/test', sess.graph)
+        train_writer = tf.summary.FileWriter(SUMMARY_DIR + '/train', sess.graph)
+        test_writer = tf.summary.FileWriter(SUMMARY_DIR + '/test')
 
         correct_prediction = tf.equal(tf.sign(y - 0.5), tf.sign(y_ - 0.5))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        train_accuracy_summary = tf.scalar_summary('accuracy', accuracy)
-        test_accuracy_summary = tf.scalar_summary('accuracy', accuracy)
+        accuracy_summary = tf.summary.scalar('accuracy', accuracy)
 
-        sess.run(tf.initialize_all_variables())
+        sess.run(tf.global_variables_initializer())
         for i in range(TRAINING_LOOP + 1):
-            images, labels = mnist.train.next_batch(BATCH_SIZE)
-            labels = [[1] if l[0] == 1 else [0] for l in labels]
+            images, org_labels = mnist.train.next_batch(BATCH_SIZE)
+            labels = gen_label(org_labels)
             sess.run(train_step, {x: images, y_: labels})
 
             if i % SUMMARY_INTERVAL == 0:
                 print('step %d' % i)
-                summary = sess.run(tf.merge_summary([log_likelihood_summary, train_accuracy_summary]), {x: images, y_: labels})
+                summary = sess.run(
+                        tf.summary.merge([log_likelihood_summary, accuracy_summary]),
+                        {x: images, y_: labels})
                 train_writer.add_summary(summary, i)
-                summary = sess.run(tf.merge_summary([test_accuracy_summary]), {x: mnist.test.images, y_: [[1] if l[0] == 1 else [0] for l in mnist.test.labels]})
+                summary = sess.run(
+                        tf.summary.merge([accuracy_summary]),
+                        {x: mnist.test.images, y_: gen_label(mnist.test.labels)})
                 test_writer.add_summary(summary, i)
